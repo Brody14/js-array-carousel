272bd55c0b95aa27ae8b244847a3efa29af41330
Brody14 <stefania.spinas@gmail.com>
2023-02-06 18:47:30 +0100

    FEAT array

--- js/script.js
+++ js/script.js
@@ -4,51 +4,77 @@
 
 // recupero elementi html
 
-const slideElement = document.getElementsByClassName('slide');
+//const slideElements = document.getElementsByClassName('slide');
 //console.log(slideElement);
 
-const arrowLeft = document.querySelector('.arrow.arrow-left');
-//console.log(arrowLeft);
+//CREARE ELEMENTI DA UN ARRAY
 
-const arrowRight = document.querySelector('.arrow.arrow-right');
-//console.log(arrowRight);
+const carouselElement = document.querySelector(".carousel");
 
-let slideIndex = 0;
+let slideElements = [
+	"./img/01.jpg",
+	"./img/02.jpg",
+	"./img/03.jpg",
+	"./img/04.jpg",
+	"./img/05.jpg",
+	"./img/06.jpg",
+];
+
 
 // aggiungo evento al click
 
-arrowRight.addEventListener ('click', function() {
-    //console.log('Filloy fa schifo');
 
-    if(slideIndex < 5) {
-
-        // cambio classi alle immagini
-        let slideActive = slideElement[slideIndex];
-        slideActive.classList.remove('active');
-
-        slideIndex += 1
-
-        let slideNext = slideElement[slideIndex];
-        slideNext.classList.add('active');
+for (let i = 0; i < slideElements.length; i++) {
+    
+    let slide = document.createElement('div');
+    slide.classList.add('slide');
+    
+    if(i === 0) {
+        slide.classList.add('active')
     }
+    
+    let slideImg = slideElements[i];   
+    
+    slide.innerHTML = `<img src=${slideImg}>`;
 
-})
-
-arrowLeft.addEventListener ('click', function() {
-    //console.log('Filloy fa schifo');
-
-    if(slideIndex > 0) {
-
-        // cambio classi alle immagini
-        let slideActive = slideElement[slideIndex];
-        slideActive.classList.remove('active');
-
-        slideIndex -= 1
-
-        let slideNext = slideElement[slideIndex];
-        slideNext.classList.add('active');
-    }
-
-})
+    carouselElement.appendChild(slide);
+}
 
 
+const arrowLeft = document.querySelector(".arrow.arrow-left");
+const arrowRight = document.querySelector(".arrow.arrow-right");
+
+let slides = document.querySelectorAll('.slide');
+let slideIndex = 0;
+
+arrowRight.addEventListener("click", function () {
+     //console.log("Filloy fa schifo");
+ 
+      if (slideIndex < 5) {
+          // cambio classi alle immagini
+          let slideActive = slides[slideIndex];
+          slideActive.classList.remove("active");
+    
+          slideIndex += 1;
+    
+          let slideNext = slides[slideIndex];
+          slideNext.classList.add("active");
+      }
+
+ })
+
+
+ arrowLeft.addEventListener("click", function () {
+ 	//console.log('Anche la virtus fa schifo');
+
+ 	if (slideIndex > 0) {
+ 		// cambio classi alle immagini
+ 		let slideActive = slides[slideIndex];
+ 		slideActive.classList.remove("active");
+
+ 		slideIndex -= 1;
+
+ 		let slideNext = slides[slideIndex];
+ 		slideNext.classList.add("active");
+ 	}
+ });
