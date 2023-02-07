502d1cb802c7fa956d3f7cc1c36a01a657531c96
Brody14 <stefania.spinas@gmail.com>
2023-02-07 11:36:53 +0100

    bonus

---
+++ bonus/js/script.js
@@ -0,0 +1,83 @@
+//console.log('Daum');
+
+// AL CLICK DELL'UTENTE SULLA FRECCIA CAMBIA IMMAGINE
+
+// recupero elementi html
+
+//const slideElements = document.getElementsByClassName('slide');
+//console.log(slideElement);
+
+//CREARE ELEMENTI DA UN ARRAY
+
+const carouselElement = document.querySelector(".carousel");
+
+let slideElements = [
+	"./img/01.jpg",
+	"./img/02.jpg",
+	"./img/03.jpg",
+	"./img/04.jpg",
+	"./img/05.jpg",
+	"./img/06.jpg",
+];
+
+
+// aggiungo evento al click
+
+
+for (let i = 0; i < slideElements.length; i++) {
+    
+    let slide = document.createElement('div');
+    slide.classList.add('slide');
+    
+    if(i === 0) {
+        slide.classList.add('active')
+    }
+    
+    let slideImg = slideElements[i];   
+    
+    slide.innerHTML = `<img src=${slideImg}>`;
+
+    carouselElement.appendChild(slide);
+}
+
+
+const arrowLeft = document.querySelector(".arrow.arrow-left");
+const arrowRight = document.querySelector(".arrow.arrow-right");
+
+let slides = document.querySelectorAll('.slide');
+let slideIndex = 0;
+
+arrowRight.addEventListener("click", function () {
+     //console.log("Filloy fa schifo");
+     let slideActive = slides[slideIndex];
+     slideActive.classList.remove("active");
+ 
+      if (slideIndex < slideElements.length - 1) {
+          slideIndex += 1;
+        } else { 
+            
+            slideIndex = 0;
+        }
+
+        let slideNext = slides[slideIndex];
+        slideNext.classList.add("active");
+
+ })
+
+
+ arrowLeft.addEventListener("click", function () {
+ 	//console.log('Anche la virtus fa schifo');
+     let slideActive = slides[slideIndex];
+     slideActive.classList.remove("active");
+     
+ 	if (slideIndex > 0) {
+         
+ 		slideIndex -= 1;
+        
+    } else {
+        slideIndex = slideElements.length - 1;
+    }
+
+    let slideNext = slides[slideIndex];
+    slideNext.classList.add("active");
+});
